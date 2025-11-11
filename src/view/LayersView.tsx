276f1244e1daeab9628471421bbf7b6fa4296b51
sliwia <liwia.szajner@gmail.com>
2025-11-11 18:46:59 +0100

    added lines to map, adjusted mask

--- src/view/LayersView.tsx
+++ src/view/LayersView.tsx
@@ -6,21 +6,24 @@ import {
   FeatureGroup,
 } from "react-leaflet";
 import { latLngBounds } from "leaflet";
+// import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-import linesGeoJson from "../data/linie2.geo.json";
 import polandGeoJson from "../data/polska.geo.json";
 import wojewodztwaGeoJson from "../data/wojewodztwa.geo.json";
+import lines from "../data/linie84.geo.json";
 import { wordCoordinates } from "../data/worldCoordinates";
 
 const polandBounds = latLngBounds([49.0, 14.1], [55.0, 24.2]);
+// Uncomment below to use canvas renderer for lines
+// const canvasRenderer = L.canvas({ padding: 0.5 });
 
 export default function MapWithStatistic() {
   const mask = {
     type: "Feature",
     geometry: {
       type: "Polygon",
-      coordinates: [wordCoordinates, polandGeoJson.geometry.coordinates[0]],
+      coordinates: [wordCoordinates, polandGeoJson.features[0].geometry.coordinates[0][0]],
     },
   };
 
@@ -51,6 +54,19 @@ export default function MapWithStatistic() {
             />
           </FeatureGroup>
         </LayersControl.Overlay>
+        <LayersControl.Overlay name="Linie">
+          <FeatureGroup>
+            <GeoJSON
+              data={lines as any}
+              // renderer={canvasRenderer}
+              style={{
+                color: "#007BFF",
+                weight: 1,
+                fillOpacity: 0.8,
+              }}
+            />
+          </FeatureGroup>
+        </LayersControl.Overlay>
 
         <LayersControl.Overlay name="Wojewodztwa" checked>
           <FeatureGroup pathOptions={{ color: "purple" }}>
@@ -66,19 +82,7 @@ export default function MapWithStatistic() {
           </FeatureGroup>
         </LayersControl.Overlay>
 
-        <LayersControl.Overlay name="Rzeki">
-          <FeatureGroup>
-            <GeoJSON
-              data={linesGeoJson as any}
-              style={{
-                color: "#007BFF",
-                weight: 2,
-                opacity: 0.8,
-                dashArray: "5,5",
-              }}
-            />
-          </FeatureGroup>
-        </LayersControl.Overlay>
+        
       </LayersControl>
     </MapContainer>
   );
